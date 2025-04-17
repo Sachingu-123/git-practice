7c54c837968cde6caa2136a0c4ca12c5198e2e18
Sachin Gupta <sachingu1620@gmail.com>
2025-04-17 18:54:58 +0530

    added sum method to the prgm

---
+++ fun.java
@@ -0,0 +1,13 @@
+public class fun{
+	public static void main(String[] args){
+		sum();
+		
+		public static void sum(){
+			int a=10;
+			int b=20;
+			System.out.println(a+b);		
+			}
+		
+		}
+
+	}
